a94ab0d11892b9bd9b4a649951b86a09e0b0626b
Fenil Narola <fenilnarola0210@gmail.com>
2023-09-07 04:38:13 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 const Navbar = () => {
   return (
     <nav className="container">
-        <div className="logo"><img src="/images/brand_logo.png" alt="Logo" /></div>
+      <div className="logo"><img src="/images/brand_logo.png" alt="Logo" /></div>
 
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
 
-        <button>Login</button>
-      </nav>
+      <button>Login</button>
+    </nav>
   )
 }
 
